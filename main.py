2a4e84c3f87199d5991913857a29237a7116ab4f
Erick <erick.azevedo7@hotmail.com>
2024-12-01 03:13:35 -0300

    feat: add pre-process and index

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-from utils import zipfGrapgh
-path = 'pan-plagiarism-corpus-2011/pan-plagiarism-corpus-2011/external-detection-corpus/source-document'
+from utils import zipfGrapgh, fileReader, folderReader, preProcess, indexDoc, preProcessTest, indexTest
+import xml.etree.ElementTree as ET
+source = 'pan-plagiarism-corpus-2011/pan-plagiarism-corpus-2011/external-detection-corpus/source-document'
+suspect = 'pan-plagiarism-corpus-2011/pan-plagiarism-corpus-2011/external-detection-corpus/suspicious-document'
 
 if __name__ == '__main__':
-    zipfGrapgh(path)
+    files = folderReader(source)
+
+    file = preProcessTest('Testing is testing and testing')
+
+    vector = ['testing', 'is', 'testing', 'and', 'testing']
+
+    indexTest(file)
+    indexTest(vector)
+
+    # indexDoc(files, schema)
--- utils.py
+++ utils.py
@@ -1,26 +1,42 @@
 import nltk
-nltk.download('stopwords')
+import whoosh
+import xml.etree.ElementTree as ET
+from whoosh.analysis import StandardAnalyzer
+from whoosh.fields import Schema, STORED, TEXT, ID
+import whoosh.fields
+import whoosh.index as index
 import os, glob
+
+import whoosh.reading
 from utils import *
 from nltk.probability import FreqDist
 from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 import string
 import matplotlib.pyplot as plt
 
-def folderReader(path, encoding='utf-8'):
-    os.chdir(path)
+def folderReader(path):
     files = []
-    for file in glob.glob("*/*.txt"):
-        files.append(file) 
+    for file in glob.glob(os.path.join(path, "*/*.txt")):
+        base = os.path.splitext(file)[0]
+        xmlfile = os.path.join(base + ".xml")
+        document = [file, xmlfile]
+
+        files.append(document)
+        break
     return files
+
+def fileReader(file, encoding='utf-8'):
+    with open(file, 'r', encoding=encoding) as f:
+        text = f.read()
+    return text
 
 def zipfGrapgh(path, encoding='utf-8'):
     fd = FreqDist()
     files = folderReader(path)
 
     for file in files:
-        with open(file, 'r', encoding=encoding) as f:
-            text = f.read()
+        text = fileReader(file[0], encoding)
         text = text.translate(str.maketrans('','',string.punctuation))
         fd += FreqDist(word.lower() for word in text.split())
 
@@ -34,3 +50,36 @@ def zipfGrapgh(path, encoding='utf-8'):
     p.set_xlabel("Amostra")
     p.set_ylabel("Frequência")
     plt.show()
+
+
+def preProcess(text):
+    stop_en = stopwords.words('english')
+    analyzer = StandardAnalyzer(stoplist=stop_en)
+    tokenVector = [token.text for token in analyzer(text)]
+
+    return tokenVector
+
+
+def indexDoc(files):
+    schema = Schema(title=STORED, reference=ID(stored=True), content=TEXT(vector=True))
+
+    if not os.path.exists("index"):
+        os.mkdir("index")
+
+    ix = index.create_in("index", schema)
+
+    writer = ix.writer()
+
+    for file in files:
+        rawText = fileReader(file[0])
+        text = preProcess(rawText)
+
+        tree = ET.parse(file[1]).getroot()
+        
+        reference = tree.get('reference')
+
+        title = tree.find('feature').get('title')
+        
+        writer.add_document(title=title, reference=reference, content=text)
+
+    writer.commit()
